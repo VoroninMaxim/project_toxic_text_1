eff081c70f38af2d3082bf1c2b71ea192026b57b
VoroninMaxim <maksimsazonov476@gmail.com>
2023-12-20 15:25:05 +0300

    Update project_start.py

--- project_start.py
+++ project_start.py
@@ -1,14 +1,30 @@
 
-from transformers import pipeline
+###!pip install transformers sentencepiece --quiet
+###-https://huggingface.co/cointegrated/rubert-tiny-toxicity
 
-clf = pipeline(task= 'sentiment-analysis', model='s-nlp/russian_toxicity_classifier')
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-text = ['Просто займись делом, и всё как рукой снимет.', 'Какая гадость эта ваша заливная рыба!',
+model_checkpoint = 'cointegrated/rubert-tiny-toxicity'
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint)
+if torch.cuda.is_available():
+    model.cuda()
+
+def text2toxicity(text, aggregate=True):
+    """ Calculate toxicity of a text (if aggregate=True) or a vector of toxicity aspects (if aggregate=False)"""
+    with torch.no_grad():
+        inputs = tokenizer(text, return_tensors='pt', truncation=True, padding=True).to(model.device)
+        proba = torch.sigmoid(model(**inputs).logits).cpu().numpy()
+    if isinstance(text, str):
+        proba = proba[0]
+    if aggregate:
+        return 1 - proba.T[0] * (1 - proba.T[-1])
+    return proba
+
+mylist = ['я люблю нигеров', 'Какая гадость эта ваша заливная рыба!',
         'Нет, не хочу ни говорить, ни слушать.', 'Не приписывай самому себе того, что не тебе принадлежит.']
-#----------------------------
-def data(text):
-    for row in text:
-        yield row
-#----------------------------
-for out in clf(data(text)):
-    print(out)
+
+# #----------------------------
+for i in mylist:
+    print(f"{i} --", "{:.6f}".format(text2toxicity(i)), "--non-toxic--")
